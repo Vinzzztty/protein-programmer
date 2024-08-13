77992c1b7aa7b47698db5a52d1056193156f8912
Vinzzztty <kevinarnandes21@gmail.com>
2024-08-13 12:50:42 +0700

    init: 30 days pandas and complete day 1

---
+++ 30-days-pandas-leetcode/day-1-Big-Countries/soluton.py
@@ -0,0 +1,46 @@
+"""
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+
+A country is big if:
+
+1. it has an area of at least three million (i.e., 3000000 km2), or
+2. it has a population of at least twenty-five million (i.e., 25000000).
+
+Write a solution to find the name, population, and area of the big countries.
+
+Return the result table in any order.
+
+The result format is in the following example.
+"""
+
+import pandas as pd
+
+# Creating the DataFrame
+data = {
+    "name": ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola"],
+    "continent": ["Asia", "Europe", "Africa", "Europe", "Africa"],
+    "area": [652230, 28748, 2381741, 468, 1246700],
+    "population": [25500100, 2831741, 37100000, 78115, 20609294],
+    "gdp": [20343000000, 12960000000, 188681000000, 3712000000, 100990000000],
+}
+
+world_df = pd.DataFrame(data)
+
+
+def big_countries(world):
+    return world[(world.area >= 3000000) | (world.population >= 25000000)][
+        ["name", "population", "area"]
+    ]
+
+
+print(big_countries(world_df))
