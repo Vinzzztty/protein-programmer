222a39d74d80489e9ca073eb9c69e0f281eba85a
Vinzzztty <kevinarnandes21@gmail.com>
2024-08-25 09:17:01 +0700

    complete: 5 introduction pandas

---
+++ introduction-pandas/5-create-new-column/solution.py
@@ -0,0 +1,60 @@
+"""
+DataFrame employees
++-------------+--------+
+| Column Name | Type.  |
++-------------+--------+
+| name        | object |
+| salary      | int.   |
++-------------+--------+
+A company plans to provide its employees with a bonus.
+
+Write a solution to create a new column name bonus that contains the doubled values of the salary column.
+
+Example 1:
+
+Input:
+DataFrame employees
++---------+--------+
+| name    | salary |
++---------+--------+
+| Piper   | 4548   |
+| Grace   | 28150  |
+| Georgia | 1103   |
+| Willow  | 6593   |
+| Finn    | 74576  |
+| Thomas  | 24433  |
++---------+--------+
+Output:
++---------+--------+--------+
+| name    | salary | bonus  |
++---------+--------+--------+
+| Piper   | 4548   | 9096   |
+| Grace   | 28150  | 56300  |
+| Georgia | 1103   | 2206   |
+| Willow  | 6593   | 13186  |
+| Finn    | 74576  | 149152 |
+| Thomas  | 24433  | 48866  |
++---------+--------+--------+
+"""
+
+import pandas as pd
+
+data = {
+    "name": ["Piper", "Grace", "Georgia", "Willow", "Finn", "Thomas"],
+    "salary": [4584, 28150, 1103, 6593, 74576, 24433],
+}
+
+employee_df = pd.DataFrame(data)
+
+
+def createBonusColumn(employees):
+    # initialize new Column Bonus
+    employees["bonus"] = 0
+
+    # add bonus from doubling the value column salary
+    employees["bonus"] = employees["salary"] * 2
+
+    return employees
+
+
+print(createBonusColumn(employee_df))
