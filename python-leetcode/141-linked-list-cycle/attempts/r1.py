5fa70f10e8cdb9722f89f18051bd04d3ccbee744
Vinzzztty <kevinarnandes21@gmail.com>
2026-05-30 15:06:41 +0700

    add: solution testing

--- python-leetcode/141-linked-list-cycle/attempts/r1.py
+++ python-leetcode/141-linked-list-cycle/attempts/r1.py
@@ -19,3 +19,18 @@ class Solution:
             current = current.next
 
         return False
+
+
+# Test
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2  # pos = 1 (balik ke node2)
+
+sol = Solution()
+print(sol.hasCycle(node1))  # True
--- python-leetcode/141-linked-list-cycle/attempts/r2.py
+++ python-leetcode/141-linked-list-cycle/attempts/r2.py
@@ -2,11 +2,13 @@
 
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -20,8 +22,20 @@ class Solution:
 
             if slow == fast:
                 return True
-        
+
         return False
 
+
+# Test
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = None  # gak ada cycle
+
 sol = Solution()
-print(sol.hasCycle([3,2,0,-4], 1))
+print(sol.hasCycle(node1))
